9f7264485d4683119c6d86fa2209c09f12b45151
Morgan <morgan.guichard@epitech.eu>
2025-02-01 15:48:06 +0100

    CLEAN CODE modular drawing function for SFML AND NCURSE

---
+++ src/Data/DataContainer.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2025
+** PDGRUSH3
+** File description:
+** DataContainer
+*/
+
+#ifndef DATACONTAINER_HPP_
+#define DATACONTAINER_HPP_
+
+#include <iostream>
+#include <string>
+
+class DataContainer {
+    public:
+        DataContainer(int x = 0, int y = 0, std::string &value = (std::string &)"") : x(x), y(y), value(value) {};
+        DataContainer() = default;
+        ~DataContainer() = default;
+        int x;
+        int y;
+        std::string &value;
+    protected:
+    private:
+};
+
+#endif /* !DATACONTAINER_HPP_ */
