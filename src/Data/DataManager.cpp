d7f3b28bd69a2c0bc0d093a3f2b41f0c1ebefc3e
lilyan-bouyer <lilyan.bouyer@epitech.eu>
2025-02-01 14:24:38 +0100

    [ADD] get data fonction structure [ADD] get for : host name username osname kernelversion date hour minute and second.

--- src/Data/DataManager.cpp
+++ src/Data/DataManager.cpp
@@ -7,49 +7,42 @@
 
 #include "DataManager.hpp"
 
-template <typename T>
-Datadictionnary<T>::Datadictionnary()
-{
-    data = nullptr;
-    type = nullptr;
-    next = nullptr;
-}
+// Fonctions for the Datadictionnary class
 
-template <typename T>
-Datadictionnary<T>::Datadictionnary(T data, DataType type)
+Datadictionnary::Datadictionnary(std::string data, DataType type)
 {
     this->data = data;
     this->type = type;
     next = nullptr;
 }
 
-template <typename T>
-Datadictionnary<T>::~Datadictionnary()
+Datadictionnary::~Datadictionnary()
 {
 }
 
-template <typename T>
-DataManager<T>::DataManager()
+
+// Fonctions for the DataManager class
+
+DataManager::DataManager()
 {
     _data = nullptr;
+    getDatavalues();
 }
 
-template <typename T>
-DataManager<T>::~DataManager()
+DataManager::~DataManager()
 {
-    Datadictionnary<T> *tmp = _data;
+    Datadictionnary *tmp = _data;
     while (tmp) {
-        Datadictionnary<T> *next = tmp->next;
+        Datadictionnary *next = tmp->next;
         delete tmp;
         tmp = next;
     }
 }
 
-template <typename T>
-void DataManager<T>::addData(DataType type, T data)
+void DataManager::addData(DataType type, std::string data)
 {
-    Datadictionnary<T> *newData = new Datadictionnary<T>(data, type);
-    Datadictionnary<T> *tmp = _data;
+    Datadictionnary *newData = new Datadictionnary(data, type);
+    Datadictionnary *tmp = _data;
     while (tmp && tmp->next) {
         tmp = tmp->next;
     }
@@ -59,11 +52,10 @@ void DataManager<T>::addData(DataType type, T data)
         _data = newData;
 }
 
-template <typename T>
-void DataManager<T>::removeData(DataType type)
+void DataManager::removeData(DataType type)
 {
-    Datadictionnary<T> *tmp = _data;
-    Datadictionnary<T> *prev = nullptr;
+    Datadictionnary *tmp = _data;
+    Datadictionnary *prev = nullptr;
     while (tmp && tmp->type != type) {
         prev = tmp;
         tmp = tmp->next;
@@ -77,22 +69,22 @@ void DataManager<T>::removeData(DataType type)
     delete tmp;
 }
 
-template <typename T>
-void DataManager<T>::setData(DataType type, T data)
+void DataManager::setData(DataType type, std::string data)
 {
-    Datadictionnary<T> *tmp = _data;
+    Datadictionnary *tmp = _data;
     while (tmp && tmp->type != type) {
         tmp = tmp->next;
     }
-    if (!tmp)
+    if (!tmp) {
         addData(type, data);
+        return;
+    }
     tmp->data = data;
 }
 
-template <typename T>
-T DataManager<T>::getData(DataType type)
+std::string DataManager::getData(DataType type)
 {
-    Datadictionnary<T> *tmp = _data;
+    Datadictionnary *tmp = _data;
     while (tmp && tmp->type != type) {
         tmp = tmp->next;
     }
@@ -100,3 +92,115 @@ T DataManager<T>::getData(DataType type)
         return nullptr;
     return tmp->data;
 }
+
+void DataManager::Refresh()
+{
+    getDatavalues();
+}
+
+void DataManager::getDatavalues()
+{
+    setData(Hostname, getHostname());
+    setData(username, getUsername());
+    setData(OS_name, getOSName());
+    setData(kernel_version, getKernelVersion());
+    setData(Date, getDate());
+    setData(Hour, getHour());
+    setData(Minute, getMinute());
+    setData(Second, getSecond());
+    // setData(Uptime, getUptime());
+    // setData(CPU, getCPU());
+    // setData(RAM, getRAM());
+    // setData(Swap, getSwap());
+    // setData(Disk, getDisk());
+    // setData(Network, getNetwork());
+    // setData(Temperature, getTemperature());
+    // setData(Fan, getFan());
+    // setData(Voltage, getVoltage());
+    // setData(Load, getLoad());
+    // setData(Process, getProcess());
+    // setData(User, getUser());
+    // setData(Root, getRoot());
+}
+
+std::string DataManager::getHostname()
+{
+    char hostname[1024];
+    gethostname(hostname, 1024);
+    return hostname;
+}
+
+std::string DataManager::getUsername()
+{
+    struct passwd *pw = getpwuid(getuid());
+    return (pw) ? std::string(pw->pw_name) : "Unknown";
+}
+
+std::string DataManager::getOSName()
+{
+    struct utsname buffer;
+
+    if (uname(&buffer) == 0) {
+        return buffer.sysname;
+    } else {
+        return "Unknown";
+    }
+}
+
+std::string DataManager::getKernelVersion()
+{
+    struct utsname buffer;
+
+    if (uname(&buffer) == 0) {
+        return buffer.release;
+    } else {
+        return "Unknown";
+    }
+}
+
+std::string DataManager::getDate()
+{
+    std::time_t t = std::time(nullptr);
+    char buffer[100];
+    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d", std::localtime(&t));
+    return std::string(buffer);
+}
+
+std::string DataManager::getHour()
+{
+    std::time_t t = std::time(nullptr);
+    char buffer[100];
+    std::strftime(buffer, sizeof(buffer), "%H", std::localtime(&t));
+    return std::string(buffer);
+}
+
+std::string DataManager::getMinute()
+{
+    std::time_t t = std::time(nullptr);
+    char buffer[100];
+    std::strftime(buffer, sizeof(buffer), "%M", std::localtime(&t));
+    return std::string(buffer);
+}
+
+std::string DataManager::getSecond()
+{
+    std::time_t t = std::time(nullptr);
+    char buffer[100];
+    std::strftime(buffer, sizeof(buffer), "%S", std::localtime(&t));
+    return std::string(buffer);
+}
+
+// int main()
+// {
+//     DataManager dataManager;
+//     dataManager.Refresh();
+//     std::cout << dataManager.getData(Hostname) << std::endl;
+//     std::cout << dataManager.getData(username) << std::endl;
+//     std::cout << dataManager.getData(OS_name) << std::endl;
+//     std::cout << dataManager.getData(kernel_version) << std::endl;
+//     std::cout << dataManager.getData(Date) << std::endl;
+//     std::cout << dataManager.getData(Hour) << std::endl;
+//     std::cout << dataManager.getData(Minute) << std::endl;
+//     std::cout << dataManager.getData(Second) << std::endl;
+//     return 0;
+// }
--- src/Data/DataManager.hpp
+++ src/Data/DataManager.hpp
@@ -6,6 +6,15 @@
 */
 
 #pragma once
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <algorithm>
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/utsname.h>
+#include <ctime>
 
 enum DataType {
     Hostname,
@@ -31,28 +40,49 @@ enum DataType {
     Root
 };
 
-template <typename T>
 class Datadictionnary {
   public:
-    Datadictionnary();
-    Datadictionnary(T data, DataType type);
+    Datadictionnary(std::string data, DataType type);
     ~Datadictionnary();
-    T *data;
+    std::string data;
     DataType type;
     Datadictionnary *next;
 };
 
-template <typename T>
 class DataManager {
   public:
     DataManager();
     ~DataManager();
-    void addData(DataType type, T data);
+    void addData(DataType type, std::string data);
     void removeData(DataType type);
-    void setData(DataType type, T data);
-    T getData(DataType type);
+    void setData(DataType type, std::string data);
+    std::string getData(DataType type);
+    void getDatavalues();
+    void Refresh();
 
   protected:
   private:
-    Datadictionnary<T> *_data;
+    Datadictionnary *_data;
+    std::string getHostname();
+    std::string getUsername();
+    std::string getOSName();
+    std::string getKernelVersion();
+    std::string getDate();
+    std::string getHour();
+    std::string getMinute();
+    std::string getSecond();
+    // std::string getUptime();
+    // std::string getCPU();
+    // std::string getRAM();
+    // std::string getSwap();
+    // std::string getDisk();
+    // std::string getNetwork();
+    // std::string getTemperature();
+    // std::string getFan();
+    // std::string getVoltage();
+    // std::string getLoad();
+    // std::string getProcess();
+    // std::string getUser();
+    // std::string getRoot();
+
 };
