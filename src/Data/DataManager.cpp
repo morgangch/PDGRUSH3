1da26f1575720305f9da21fd04c67e7d15557f77
lilyan-bouyer <lilyan.bouyer@epitech.eu>
2025-02-01 11:53:02 +0100

    [ADD] struct for data manager.

---
+++ src/Data/DataManager.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2025
+** B-PDG-300-MPL-3-1-PDGRUSH3-morgan.guichard
+** File description:
+** DataManager.hpp
+*/
+
+ #include "DataManager.hpp"
---
+++ src/Data/DataManager.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** B-PDG-300-MPL-3-1-PDGRUSH3-morgan.guichard
+** File description:
+** DataManager.hpp
+*/
+
+#pragma once
+
+class DataManager {
+    public:
+        DataManager();
+        ~DataManager();
+
+    protected:
+    private:
+};
